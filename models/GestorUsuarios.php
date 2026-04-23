97521df5e4edd7a74b1e9a6c9e5e0c676ccbd8b9
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-23 18:04:02 +0200

    Subiendo archivo GestorUsuarios
    Contiene la conexion a la BD usando silglenton
    
    Un inicio de sesion y un registro del cliente con la password encryptada

---
+++ models/GestorUsuarios.php
@@ -0,0 +1,36 @@
+<?php
+
+class GestorUsuarios{
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db= Connection::getInstance()->getConn();
+    }
+
+    public function registrarCliente($nombre,$email,$password,$departamento,$telefono){
+        $passwordEncryptada= password_hash($password, PASSWORD_DEFAULT);
+
+        $sql="INSERT INTO usuarios (nombre, email, password, rol, departamento, telefono) VALUES(?,?,?,'cliente',?,?)";
+
+        $stmt=$this->db->prepare($sql);
+
+        return $stmt->execute([$nombre,$email,$passwordEncryptada,$departamento,$telefono]);
+
+        }
+    
+
+    public function verificarLogin($email, $password){
+        $sql= "SELECT * FROM usuarios WHERE email= ?";
+        $stmt= $this->db->prepare($sql);
+        $stmt->execute([$email]);
+        $datosUsuario =$stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($datosUsuario && password_verify($password,$datosUsuario['password'])){
+            return $datosUsuario;
+        }
+        return false;
+    }
+
+}
