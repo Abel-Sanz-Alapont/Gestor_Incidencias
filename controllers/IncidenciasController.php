6d9102ea36323ecee4d67db3ac8411af9b365975
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-23 19:37:36 +0200

    Subiendo archivo Incidencias Controller

---
+++ controllers/IncidenciasController.php
@@ -0,0 +1,46 @@
+<?php
+
+class IncidenciasController
+{
+    private $gestor;
+
+    public function __construct()
+    {
+        $this->gestor = new IncidenciasManager;
+    }
+
+    public function listar()
+    {
+
+        if (!isset($_SESSION['id'])) {
+            header("Location: index.php?accion=login");
+            exit;
+        }
+        $id_usuario = $_SESSION['id'];
+        $rol = $_SESSION['rol'];
+        $nombre = $_SESSION['nombre'];
+
+        $incidencias = $this->gestor->obtenerIncidencias($id_usuario, $rol);
+        require_once 'views/listar_incidencias.php';
+    }
+
+    public function crear()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $titulo = $_POST['titulo'];
+            $descripcion = $_POST['descripcion'];
+
+            $id_cliente = $_SESSION['id'];
+
+            if ($this->gestor->crearIncidencia($id_cliente, $titulo, $descripcion)) {
+                header("Location: index.php?accion=listar");
+                exit();
+            } else {
+                require_once "views/crear_incidencia.php";
+            }
+        } else {
+
+            require_once "views/crear_incidencia.php";
+        }
+    }
+}
