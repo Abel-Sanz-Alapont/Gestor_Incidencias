9740d9f16173b117b98c51fb4728b06ff4161c24
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-23 15:41:09 +0200

    Subiendo clase Padre

---
+++ models/Usuarios.php
@@ -0,0 +1,88 @@
+<?php
+
+class Usuarios{
+    protected $id;
+    protected $nombre;
+    protected $email;
+    protected $rol;
+
+    public function __construct($id,$nombre,$email,$rol){
+        
+        $this->id=$id;
+        $this->nombre=$nombre;
+        $this->email=$email;
+        $this->rol=$rol;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+       
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+       
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        
+    }
+
+    /**
+     * Get the value of rol
+     */
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    /**
+     * Set the value of rol
+     */
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+
+        
+    }
+}
