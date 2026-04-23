494f6e645f21da8662f97d88149d1a79822c3b03
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-23 19:34:51 +0200

    Añadiendo Metodo para crear incidencias
    Ademas de algunos errores en la setencia de SQL  ya que me faltaba algun caracter

--- models/IncidenciasManager.php
+++ models/IncidenciasManager.php
@@ -1,29 +1,45 @@
 <?php
 
-class IncidenciasManager{
+class IncidenciasManager
+{
     private $db;
 
-       public function __construct(){   
+    public function __construct()
+    {
         // conexion Singleton
-        $this->db= Connection::getInstance()->getConn();
+        $this->db = Connection::getInstance()->getConn();
     }
 
     // Metodo de obtener incicendias dependiendo del usuario
-    public function obtenerIncidencias($id_usuario, $rol){
+    public function obtenerIncidencias($id_usuario, $rol)
+    {
         //si es administrador ve todo 
-        if ($rol=== 'administrador') {
-            $sql = "SELECT incidiencias.*, usuarios.nombre FROM incidencias INNER JOIN usuarios ON incidencias.id_cliente = usuarios.id ORDER BY incidencias.id DESC ";
+        if ($rol === 'administrador') {
+            $sql = "SELECT incidencias.*, usuarios.nombre AS nombre_cliente 
+                    FROM incidencias 
+                    INNER JOIN usuarios ON incidencias.id_cliente = usuarios.id 
+                    ORDER BY incidencias.id DESC";
 
-            $stmt=$this->db->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute();
-
-        }else{
+        } else {
             // si es cliente solo ve sus propias incidencias
-            $sql="SELECT * FROM incidencias WHERE id_cliente = ? ORDER BY id DESC";
-            $stmt=$this->db->prepare($sql);
-            $stmt->execute();
+            $sql = "SELECT * FROM incidencias WHERE id_cliente = ? ORDER BY id DESC";
+            $stmt = $this->db->prepare($sql);
 
+            $stmt->execute([$id_usuario]);
         }
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    
+    public function crearIncidencia($id_cliente, $titulo, $descripcion)
+    {
+        $sql = "INSERT INTO incidencias (id_cliente, titulo, descripcion, estado) 
+            VALUES (?, ?, ?, 'abierta')";
+
+        $stmt = $this->db->prepare($sql);
+        
+        return $stmt->execute([$id_cliente, $titulo, $descripcion]);
     }
 }
