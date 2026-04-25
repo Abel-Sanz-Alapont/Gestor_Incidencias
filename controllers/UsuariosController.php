6743df70135ad6f2b4cd16894c70079b34a6d236
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-25 17:22:02 +0200

    Modificacion UsuariosController
    Modificado el metodo de registro para que recoga los campos de los tipos de usuarios

--- controllers/UsuariosController.php
+++ controllers/UsuariosController.php
@@ -1,67 +1,73 @@
 <?php
 
-class UsuariosController {
+class UsuariosController
+{
     private $manager;
 
-    public function __construct() {
-        
+    public function __construct()
+    {
+
         $this->manager = new GestorUsuarios();
     }
 
-   
-    public function login() {
-        
+
+    public function login()
+    {
+
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $email = $_POST['email'];
             $password = $_POST['password'];
 
-            
+
             $usuario = $this->manager->verificarLogin($email, $password);
 
             if ($usuario) {
-                
+
                 $_SESSION['id'] = $usuario['id'];
                 $_SESSION['nombre'] = $usuario['nombre'];
                 $_SESSION['rol'] = $usuario['rol'];
 
-                
+
                 header("Location: index.php?accion=listar");
                 exit();
             } else {
-                
+
                 $error = "Email o contraseña incorrectos.";
                 require_once 'views/login.php';
             }
         } else {
-            
+
             require_once 'views/login.php';
         }
     }
 
-    
-    public function registro() {
+
+    public function registro()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $nombre = $_POST['nombre'];
             $email = $_POST['email'];
             $password = $_POST['password'];
-            $departamento = $_POST['departamento'];
-            $telefono = $_POST['telefono'];
 
-            
-            if ($this->manager->registrarCliente($nombre, $email, $password, $departamento, $telefono)) {
-                
-                header("Location: index.php?accion=login");
-                exit();
+            $rol = $_POST['rol'];
+
+            if ($rol == "administrador") {
+                $numero_empleado = $_POST['numero_empleado'];
+                $especialidad = $_POST['especialidad'];
+
+                $administradorRegistro = $this->manager->registrarAdministrador($nombre, $email, $password, $numero_empleado, $especialidad);
             } else {
-                $error = "No se pudo realizar el registro. Inténtalo de nuevo.";
-                require_once 'views/registro.php';
+
+                $departamento = $_POST['departamento'];
+                $telefono = $_POST['telefono'];
+                $clienteRegistro = $this->manager->registrarAdministrador($nombre, $email, $password, $departamento, $telefono);
             }
-        } else {
-            require_once 'views/registro.php';
+            header("Location: index.php?accion=login");
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         session_destroy();
         header("Location: index.php?accion=login");
         exit();
