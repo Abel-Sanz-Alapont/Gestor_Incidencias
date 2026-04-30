5cb46841b2c472774a5aa4ddfd1552cf2effe66c
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-04-30 12:00:29 +0200

    Modificancion ene el archivo listar_incidencias
    Se a modificado el el panel de los usuarios con el $_SESSION
    
    Se añadido un panel para buscar usuarios por su identificador

--- views/listar_incidencias.php
+++ views/listar_incidencias.php
@@ -17,25 +17,36 @@ $colorActual = $_COOKIE[$nombreCookieUsuario] ?? '#ffffff';
         <header>
             <div class="usuario">
                 <h1>Panel de Incidencias</h1>
-                <p>Bienvenido, <strong><?php echo $nombre; ?></strong> (<?php echo $rol; ?>)</p>
-
-                <p>
-                
+                <div>
+                    <?php if (isset($_SESSION['id'])): ?>
+                        Bienvenido, <b><?= $_SESSION['nombre'] ?></b> (<?= $_SESSION['rol'] ?>) |
+                        <a href="index.php?accion=logout">Cerrar Sesión</a>
+                        <?php if ($_SESSION['rol'] === 'cliente'): ?>
+                            | <a href="index.php?accion=crear">Añadir Nueva Incidencia</a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
                 <form action="index.php?accion=cambiar_color" method="POST">
                     <label for="escogerColor">Seleccionar color fondo</label>
                     <input type="color" id="escogerColor" name="color" value="<?php echo $colorActual; ?>">
                     <button type="submit">Cambiar</button>
                 </form>
-                <hr>
-                <a href="index.php?accion=logout">Cerrar Sesión</a>
-                <?php if ($rol === 'cliente'): ?>
-                    | <a href="index.php?accion=crear">Añadir Nueva Incidencia</a>
-                <?php endif; ?>
-                </p>
             </div>
         </header>
 
         <main>
+            <div class= "panel-busqueda">
+                <?php if ($_SESSION['rol'] === 'administrador'): ?>
+                    <div >
+                        <form method="POST" action="index.php?accion=buscar">
+                            <label for="id_busqueda">BUSCAR ID DEL CLIENTE</label><br><hr>
+                            <input type="number" id="id_busqueda" name="id_busqueda" placeholder="Introduce ID cliente"><br><br>
+                            <button type="submit">Buscar</button>
+                            <a href="index.php?accion=listar">Listar todas</a>
+                        </form>
+                    </div>
+                <?php endif; ?>
+            </div>
             <?php if (empty($incidencias)): ?>
                 <p>No hay incidencias registradas en este momento.</p>
             <?php else: ?>
@@ -46,7 +57,7 @@ $colorActual = $_COOKIE[$nombreCookieUsuario] ?? '#ffffff';
                             <th>Título</th>
                             <th>Descripción</th>
                             <th>Acciones</th>
-                            <?php if ($rol === 'administrador'): ?>
+                            <?php if ($_SESSION['rol']  === 'administrador'): ?>
                                 <th>Cliente</th>
                             <?php endif; ?>
                         </tr>
@@ -59,7 +70,7 @@ $colorActual = $_COOKIE[$nombreCookieUsuario] ?? '#ffffff';
                                 <td><?php echo $incidencia['descripcion']; ?></td>
 
                                 <td>
-                                    <?php if ($rol === 'administrador'): ?>
+                                    <?php if ($_SESSION['rol']  === 'administrador'): ?>
                                         <form action="index.php?accion=actualizar" method="POST" style="display:inline;">
                                             <input type="hidden" name="id_incidencia" value="<?php echo $incidencia['id']; ?>">
                                             <select name="nuevo_estado">
@@ -77,7 +88,7 @@ $colorActual = $_COOKIE[$nombreCookieUsuario] ?? '#ffffff';
                                     <?php endif; ?>
                                 </td>
 
-                                <?php if ($rol === 'administrador'): ?>
+                                <?php if ($_SESSION['rol']  === 'administrador'): ?>
                                     <td><?php echo $incidencia['nombre_cliente'] ?? 'Usuario Desconocido'; ?></td>
                                 <?php endif; ?>
                             </tr>
